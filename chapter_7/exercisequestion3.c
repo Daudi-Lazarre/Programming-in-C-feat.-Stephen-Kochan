ccb29c0b3f11c473d6592af502bb8585c3958e9f
Lucas Lazarre <lucaslazarre@Lucass-MacBook-Air.local>
2021-05-10 19:41:28 -0400

    Uploading the third exercise

---
+++ chapter_7/exercisequestion3.c
@@ -0,0 +1,42 @@
+// Program demonstrating an array of counters
+
+#include <stdio.h>
+
+int main (void)
+{
+	int ratingCounters[11];
+	int i;
+	int response;
+
+	for(i = 1; i <= 10; ++i) //initializes array
+		ratingCounters[i] = 0;
+
+	printf("Enter your responses\n");
+
+	if(i)
+	{
+		scanf("%i", &response);
+
+		// Modify that program so that any
+		// number of responses can be entered.
+		// Set up the program so that the value 999 can be keyed in
+		// by the user to indicate that the last
+		// response has been entered.
+		printf("Valid response: %i\n", response);
+
+		if(response == 999)
+		{
+			printf("The last response has been entered.\n");
+		}
+		else
+			++ratingCounters[response];
+	}
+
+	printf("\n\n Rating   Number of Responses\n");
+	printf("_______ ___________________\n");
+
+	for ( i = 1; i <= 10; ++i )
+		printf("%4i%14i\n", i, ratingCounters[i]);
+
+	return 0;
+}
