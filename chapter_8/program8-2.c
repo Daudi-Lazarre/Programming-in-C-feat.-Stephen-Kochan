97a51d4250606ea7fbb925be4201544cd03d3dd9
Lucas Lazarre <lucaslazarre@Lucass-MacBook-Air.local>
2021-05-17 12:00:57 -0400

    Uploading the first two example programs

--- chapter_8/program8-2.c
+++ chapter_8/program8-2.c
@@ -2,13 +2,13 @@
 
 void printMessage(void)
 {
-	printf ("Programming is fun.\n");
+	printf("Programming is fun.\n");
 }
 
 int main (void)
 {
-	printMessage ();
-	printMessage ();
+	printMessage();
+	printMessage();
 	return 0;
 }
 // The output of this program is
