9d524840ca48a707408d7fdbc61e3dee81163397
Lucas Lazarre <lucaslazarre@Lucass-MacBook-Air.local>
2021-05-29 19:49:28 -0400

    Uploading the calculator again: I made a tiktok video with it

--- chapter_6/calculator.c
+++ chapter_6/calculator.c
@@ -12,22 +12,27 @@ int main()
 // Hold space for the user to enter that math.
 	scanf("%i %c %i", &accumulator, &ASMD, &num2);
 
+// Think of switch statements as if statements
+// If the user types in a plus operator, print the sum.
 	switch(ASMD)
 	{
 	case '+':
 		printf("The sum of your entry is %i.\n", accumulator + num2);
 		break;
 
+// If the user types in a minus operator, print the difference.
 	case '-':
 		printf("The difference of your ");
 		printf("entry is %i.\n", accumulator - num2);
 		break;
 
+// If the user types in an asterisk, print the product.
 	case '*':
 		printf("The product of your entry is ");
 		printf("%i.\n", accumulator * num2);
 		break;
 
+// If the user types in a forward slash, print the quotient.
 	case '/':
 		printf("The quotient of your entry is ");
 		printf("%i.\n", accumulator / num2);
